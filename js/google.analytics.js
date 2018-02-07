ce960f2644a9ac012d43ac8c306d2977b6a0afd4
João Britto <jabcalves@gmail.com>
2018-02-07 20:59:31 -0200

    Preparando conteúdo para habilitar CSP

---
+++ js/google.analytics.js
@@ -0,0 +1,8 @@
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-101942144-1', 'auto');
+ga('send', 'pageview');
+
+// Outbound link tracking.
+ga('require', 'outboundLinkTracker', {
+  events: ['click', 'auxclick', 'contextmenu']
+});
